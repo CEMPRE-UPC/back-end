1146243cc6e66634f202a717da3397e8c85ee506
Willinton Mora <wifermen@hotmail.com>
2023-11-04 12:27:27 -0500

    refactor: validar que no lleguen string vacios a la base de datos

--- src/domain/dtos/student/work-experience/optional-work-experience.dto.ts
+++ src/domain/dtos/student/work-experience/optional-work-experience.dto.ts
@@ -1,24 +1,28 @@
 import { Validators } from '../../../../config';
 
-
 export class OptionalWorkExperienceDto {
     constructor(
         public studentId: string,
         public id: string,
-        public company?: string,
-        public position?: string,
-        public functions?: string,
-        public start_date?: string,
-        public end_date?: string,
+        public company: string,
+        public position: string,
+        public functions: string,
+        public start_date: string,
+        public end_date: string,
     ) {}
 
+    static create(body: {[key: string]: any }, id: string): [string?, Partial<OptionalWorkExperienceDto>?] {
 
-    static create(body: {[key: string]: any }, id: string): [string?, OptionalWorkExperienceDto?] {
-
-        const { company, position, functions, start_date, end_date, studentId } = body;
+        const { studentId, ...rest } = body;
 
         if (!id) return ['id is required'];
         if (!studentId) return ['studentId is required'];
+
+        const company = body.company || undefined;
+        const position = body.position || undefined;
+        const functions = body.functions || undefined;
+        const start_date = body.start_date || undefined;
+        const end_date = body.end_date || undefined;
 
         if (start_date) {
             if(!Validators.datePattern.test(start_date)) return ['start_date is invalid'];
@@ -27,6 +31,6 @@ export class OptionalWorkExperienceDto {
             if(!Validators.datePattern.test(end_date)) return ['end_date is invalid'];
         }
 
-        return [undefined, new OptionalWorkExperienceDto(studentId, id, company, position, functions, start_date, end_date)];
+        return [undefined, new OptionalWorkExperienceDto(studentId, id, company, position, functions, start_date, end_date,)];
     }
 }
