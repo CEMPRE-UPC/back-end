10c04b314161ab7082985f618c74002f539cf4aa
Willinton Mora <wifermen@hotmail.com>
2024-04-21 15:04:14 -0500

    refactor: se eliminos la carpeta dist

--- dist/domain/types/student/level-studies.enum.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.LevelStudies = void 0;
-var LevelStudies;
-(function (LevelStudies) {
-    LevelStudies["SECONDARY"] = "Nivel secundario";
-    LevelStudies["OTHER"] = "Otros estudios";
-})(LevelStudies || (exports.LevelStudies = LevelStudies = {}));
